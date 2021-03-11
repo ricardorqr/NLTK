15c82724bfa45bcbe42f3d1e0d427148dc16fa2f
Ricardo Ribeiro <ricardo@lyricfind.com>
2021-03-11 00:06:37 -0500

    Final version

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,75 +1,102 @@
-from sklearn.datasets import fetch_20newsgroups
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-from nltk.stem.wordnet import WordNetLemmatizer
-import re
+import string
+from collections import Counter
+from collections import defaultdict
 
-text_data = fetch_20newsgroups()
-raw_text = text_data.data[:4]
-# print(raw_text)
+import matplotlib.pyplot as plt
+import nltk
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+# Get the text
+with open('resources/Steve Jobs Speech.txt', 'r') as file:
+    text_data = file.readlines()
+# print(text_data)
 
 # Convert to lower case
 clean_text_1 = []
-for words in raw_text:
-    clean_text_1.append(str.lower(words))
+for line in text_data:
+    clean_text_1.append(str.lower(line))
 # print(clean_text_1)
 
-# Tokenize
-sent_tok = []
-for sent in clean_text_1:
-    sent = sent_tokenize(sent)
-    sent_tok.append(sent)
-# print(sent_tok)
-
-
-# Word tokenize
-clean_text_2 = [word_tokenize(i) for i in clean_text_1]
+# Remove punctuation and special characters
+clean_text_2 = []
+for line in clean_text_1:
+    clean_text_2.append(line.translate(str.maketrans('', '', string.punctuation)))
 # print(clean_text_2)
 
+# Tokenize
 clean_text_3 = []
-for words in clean_text_2:
-    clean = []
-    for w in words:
-        res = re.sub(r'[^\w\s]', "", w)
-        if res != "":
-            clean.append(res)
-        clean_text_3.append(clean)
+for line in clean_text_2:
+    token = word_tokenize(line, 'english')
+    clean_text_3.append(token)
 # print(clean_text_3)
 
-
-# Stop word removal
-clean_text_4 = []
-for words in clean_text_3:
-    w = []
-    for word in words:
-        if not word in stopwords.words('english'):
-            w.append(word)
-        clean_text_4.append(w)
+# Remove empty line
+clean_text_4 = list(filter(None, clean_text_3))
 # print(clean_text_4)
 
-
-# Stemming (example 1)
+# Stopwords removal
 clean_text_5 = []
-port = PorterStemmer()
-for words in clean_text_4:
-    w = []
-    for word in words:
-        w.append(port.stem(word))
-    clean_text_5.append(w)
-# print(clean_text_5)
+for line in clean_text_4:
+    clean_word = []
 
+    for word in line:
+        if word not in stopwords.words('english'):
+            clean_word.append(word)
+    clean_text_5.append(clean_word)
+# print(clean_text_5[:10])
+
+
+def get_wordnet_pos(word_to_tag):
+    tag_word = nltk.pos_tag([word_to_tag])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ, "N": wordnet.NOUN, "V": wordnet.VERB, "R": wordnet.ADV}
+    return tag_dict.get(tag_word, wordnet.NOUN)
+
+
+# Lemmatize (example 1)
+clean_text_6 = []
+lemmatizer = WordNetLemmatizer()
+tag_map = defaultdict(lambda: wordnet.NOUN)
+tag_map['J'] = wordnet.ADJ
+tag_map['V'] = wordnet.VERB
+tag_map['R'] = wordnet.ADV
+for line in clean_text_5:
+    clean_word = []
+
+    for word in line:
+        clean_word.append(lemmatizer.lemmatize(word, get_wordnet_pos(word)))
+    clean_text_6.append(clean_word)
+# print(clean_text_6[:10])
 
 # Lemmatize (example 2)
-wnet = WordNetLemmatizer()
-lem = []
-for words in clean_text_4:
-    w = []
-    for word in words:
-        w.append(wnet.lemmatize(word))
-    lem.append(w)
-# print(lem)
+clean_text_7 = []
+for line in clean_text_6:
+    clean_word = []
 
+    for token, tag in pos_tag(line):
+        clean_word.append(lemmatizer.lemmatize(token, tag_map[tag[0]]))
+    clean_text_7.append(clean_word)
+# print(clean_text_7[:10])
 
-print(clean_text_5[:1])
-print(lem[:1])
+# Create emotion list
+emotion_list = []
+with open('resources/emotions.txt', 'r') as file:
+    for emotion_line in file:
+        clear_line = emotion_line.replace("\n", '').replace(",", '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+
+        for line in clean_text_7:
+            if word in line:
+                emotion_list.append(emotion)
+count = Counter(emotion_list)
+print(count)
+
+# Plot the analysis
+fig, ax1 = plt.subplots()
+ax1.bar(count.keys(), count.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
--- README.md
+++ README.md
@@ -2,43 +2,43 @@ This is a Sentiment Analysis project using the library NLTK in Python.
 
 ### Contents
 
-1. [NLTK](#NLTK)
+1. [NLTK](#nlp)
 1. [Pipeline](#pipeline)
-    1. [Data Cleaning](#Data-Cleaning)
-    1. [Vectorization](#Vectorization)
-    1. [Perform Classification](#Perform-Classification)
+   1. [Data Cleaning](#Data-Cleaning)
+   1. [Perform Analysis](#Perform-Analysis)
 1. [Libraries](#libraries)
-    1. [Scikit-learn](#scikit-learn)
-    1. [NLTK](#nltk)
-    1. [NLTK Data](#nltk-data)
-    1. [NLTK Stopwords Corpus](#nltk-stopwords-corpus)
-    1. [NLTK WordNet](#nltk-wordnet)
+   1. [Matplotlib](#matplotlib)
+   1. [Scikit-learn](#scikit-learn)
+   1. [NLTK](#nltk)
+   1. [NLTK Data](#nltk-data)
+   1. [NLTK Stopwords Corpus](#nltk-stopwords-corpus)
+   1. [NLTK WordNet](#nltk-wordnet)
+   1. [NLTK WordNet](#nltk-wordnet)
     
-# NLTK
+# NLP
 
 Natural Language Processing, or NLP for shot, is broadly defined as the automatic manipulation of natural language, like speech and text by software.
-In addition, Natural Language Toolkit (NLTK) is a leading platform for building Python programs to work with human language data.
 
 # Pipeline
 
 A pipeline is just a way to design a program where the output of one step is the input of the next step.
 
-1. Text Document (begin)
+1. Text Document
 1. [Data Cleaning](#Data-Cleaning)
-1. [Vectorization](#Vectorization)
-1. [Perform Classification (end)](#Perform-Classification)
+1. [Perform Analysis] (#Perform-Analysis)
 
 ## Data Cleaning
 
 Convert the raw text into a list of words that are clean text (this is a very important step).
 
 1. Data Cleaning (pre-processing)
-    1. Convert to Lower Case
-    1. Tokenization
-    1. Remove Punctuation and Special Characters
-    1. Stopwords Removal
-    1. Stemming
-    1. Lemmatization
+   
+   1. Convert to Lower Case
+   1. Remove Punctuation and Special Characters
+   1. Tokenization
+   1. Remove empty line
+   1. Stopwords Removal
+   1. Lemmatization
     
 Some definitions:
 
@@ -46,16 +46,27 @@ Some definitions:
 - Stopwords Removal - Remove words which are present in the sentence and make no difference to the analysis.
 - Stemming - Reduce the word to the base form. Ex.: Reading -> read.
 - Lemmatization - Process of grouping together the different inflected forms of a word then they can be analysed as a single item.
+   - Lemmatization runs 2 times with different parameters. That happens in order to clean words that were not clean the first time.  
 
 ## Vectorization
 
 Convert words into numbers.
 
-## Perform Classification
+## Perform Analysis
 
-Process of assigning tags or categories to the text according to its content.
+Plot the analysis. The result should be like the picture below.
+
+![Result](/resources/04.png)
 
 # Libraries
+
+## Matplotlib
+
+Run the Python interpreter and type the command:
+
+`% pip install matplotlib`
+
+Source: https://matplotlib.org/stable/users/installing.html
 
 ## scikit-learn
 
@@ -67,11 +78,13 @@ Source: https://scikit-learn.org/stable/index.html
 
 ## NLTK
 
+Natural Language Toolkit (NLTK) is a leading platform for building Python programs to work with human language data.
+
 Run the Python interpreter and type the command:
 
 `% pip install nltk`
 
-Source: https://scikit-learn.org/stable/index.html
+Source: https://www.nltk.org/install.html
 
 ## NLTK Data
 
